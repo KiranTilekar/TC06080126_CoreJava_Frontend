1a9e5d9ab0054308d9cfb58d9ffac4209366bd12
KiranTilekar <109820106+KiranTilekar@users.noreply.github.com>
2024-01-18 13:10:04 +0530

    Add files via upload

---
+++ Day3a.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Day3a {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // -------------------- For loop ------------------------
+
+        // int i,j;
+        // for(i=1;i<=5;i++)
+        // {
+        //     for(j=1;j<=i;j++)
+        //     {
+        //         System.out.print(j); //i or j
+        //     }
+        //     System.out.println("");
+        // }
+
+        // -------------------- while loop ------------------------
+
+        // int i=1;
+        // while(i<=5)
+        // {
+        //     int j=1;
+        //     while(j<=i)
+        //     {
+        //         System.out.print(i); //i or j
+        //         j++;
+        //     }
+        //     i++;
+        //     System.out.println("");
+            
+        // }
+
+        // -------------------- do-while loop ------------------------
+
+        // int i=1;
+        // do
+        // {
+        //     int j=1;
+        //     do
+        //     {
+        //         System.out.print(i); //i or j
+        //         j++;
+        //     }while(j<=i);
+        //     i++;
+        //     System.out.println("");
+            
+        // }while(i<=5);
+
+
+
+    }
+}
---
+++ Day4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Day4 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // ---------------------- OOP ---------------------------
+        // oops -> more security
+        // class : template,user define data type, collection of (var/data,method),no physical existance
+        // object : variable of class (state/member data,,identity)
+        //5 pillars
+        //constructor
+
+        // ** if we create class in source directly then it will be in default package **
+
+        //package provide visibility control
+        //package remove naming collision
+        //package: 1.user defined, pre defined
+    }
+}
