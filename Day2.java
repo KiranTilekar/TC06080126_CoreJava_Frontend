e9d029ddabca145794baaf8c828a7e52f55466ea
KiranTilekar <109820106+KiranTilekar@users.noreply.github.com>
2024-01-18 13:09:35 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,97 @@
+// import java.util.Scanner;
+// public class Day2 
+// {
+//     public static void main(String args[])
+//     {
+        // int id = 4134;             //passing value at compile time
+        // String name = "Kiran";  //null at the end of string
+        // float marks=89;
+        // System.out.println(marks);
+
+        //---------------------------------- Scanning data -------------------------------
+        // Scanner sc = new Scanner(System.in);   //scanning data from user/keyboard, new:allocate memory
+        // int roll;
+        // System.out.println("Enter roll:");
+        // roll = sc.nextInt();
+        // System.out.println("Roll="+roll);
+
+        //------------------------------------ char input ------------------------------
+        // char ch;                               //taking char input
+        // System.out.println("Enter char:");
+        // ch = sc.next().charAt(2);
+        // System.out.println("Entered char is:"+ch);
+
+        //---------------------------------- Control Statements ------------------------
+        //used for validation purpose
+        // float eng,math,hin,total,per;
+        // System.out.println("Enter marks in eng:");
+        // eng = sc.nextFloat();
+        // System.out.println("Enter marks in math:");
+        // math = sc.nextFloat();
+        // System.out.println("Enter marks in hin:");
+        // hin = sc.nextFloat();
+        // total = eng+math+hin;
+        // per = (total*100)/300;
+        // System.out.println("Percentage:"+per);
+
+                          // --------- if-else ----------
+        // if(per<=35){
+        // System.out.println("Fail");
+        // }else{
+        // System.out.println("Pass");
+        // }
+
+                         // -------- if-else-if ladder --------
+        // if(per>=75 && per<=100)
+        // {
+        // System.out.println("First Division");
+        // }
+        // else if(per>=50 && per<75)
+        // {
+        // System.out.println("Second Division");
+        // }
+        // else if(per>35 && per<50)
+        // {
+        // System.out.println("Third Division");
+        // }
+        // else
+        // {
+        // System.out.println("Fail");
+        // }
+
+                          // ------------- nested if ---------
+        //does not use else
+        // if(per>=75)
+        // {
+        //     if(per<=100)
+        //     {
+        //         System.out.println("First Division");
+        //     }
+        // }
+
+        // if(per>=50)
+        // {
+        //     if(per<75)
+        //     {
+        //         System.out.println("Second Division");
+        //     }
+        // }
+
+        // if(per>35)
+        // {
+        //     if(per<50)
+        //     {
+        //         System.out.println("Third Division");
+        //     }
+        // }
+
+        // if(per<=35)
+        // {
+        //     System.out.println("Fail");
+        // }
+
+        
+//     }
+// }
+
+
